3074bd7bc12b7f4c33a6c358b599c5e1dddfad52
Atio Wahyudi <atiowahyudi02@gmail.com>
2026-05-08 09:07:26 +0700

    add: Tugas flutter

--- lib/day_5/scaffold.dart
+++ lib/day_5/scaffold.dart
@@ -7,16 +7,48 @@ class ScaffoldDay5 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Profile Saya"),
+        title: const Text(
+          "Profil Saya",
+          style: TextStyle(
+            color: Colors.white,
+          ), // Memastikan teks berwarna putih
+        ),
         centerTitle: true,
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.red[900], // Mengubah warna AppBar menjadi merah
       ),
-      body: Column(
-        children: [
-          Row(children: [Text("Atio Wahyudi Saputra")]),
-          Row(children: [Icon(Icons.location_on), Text("Jakarta Utara")]),
-          Row(children: [Text("Seorang peserta pelatihan PPKD Jakarta Pusat")]),
-        ],
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(18),
+        // color: Colors.amber,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "Atio Wahyudi Saputra",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.amber,
+                fontSize: 30,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.location_on,
+                  size: 20,
+                ), // Mengecilkan sedikit ukuran icon
+                SizedBox(width: 4), // Jarak kecil antara icon dan teks
+                Text("Jakarta Timur", style: TextStyle(fontSize: 15)),
+              ],
+            ),
+            Text(
+              "Peserta Pelatihan PPKD",
+              style: TextStyle(color: Colors.blueGrey),
+            ),
+          ],
+        ),
       ),
     );
   }
